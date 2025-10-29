69fe54006497b09f0ffa45fd428070775d302a06
anouar eil barry <elbarryanwar37@gmail.com>
2025-10-29 14:13:10 +0100

    add enums for ColisStatus and Priority

---
+++ src/main/java/org/example/smart_delivery/entity/enums/ColisStatus.java
@@ -0,0 +1,9 @@
+package org.example.smart_delivery.entity.enums;
+
+public enum ColisStatus {
+    CREATED,
+    COLLECTED,
+    IN_STOCK,
+    IN_TRANSIT,
+    DELIVERED
+}
---
+++ src/main/java/org/example/smart_delivery/entity/enums/Priority.java
@@ -0,0 +1,7 @@
+package org.example.smart_delivery.entity.enums;
+
+public enum Priority {
+    HIGHT,
+    MEDIUM,
+    LOW
+}
