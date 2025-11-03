9a35fd8aa030a1cdb67c4f283da4e69c276c5fc7
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-03 12:06:36 +0100

    feat: add findColisByStatut method to ColisRepository for retrieving colis by status

--- src/main/java/org/example/smart_delivery/repository/ColisRepository.java
+++ src/main/java/org/example/smart_delivery/repository/ColisRepository.java
@@ -1,7 +1,11 @@
 package org.example.smart_delivery.repository;
 
 import org.example.smart_delivery.entity.Colis;
+import org.example.smart_delivery.entity.enums.ColisStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ColisRepository extends JpaRepository<Colis, String> {
+    List<Colis> findColisByStatut(ColisStatus statut);
 }
