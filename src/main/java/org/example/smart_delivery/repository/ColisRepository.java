3808c05f841f4ca86358754b6681c53626fc5e9c
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-08 15:03:58 +0100

    feat: update ColisRepository to support pagination for colis status retrieval

--- src/main/java/org/example/smart_delivery/repository/ColisRepository.java
+++ src/main/java/org/example/smart_delivery/repository/ColisRepository.java
@@ -2,10 +2,12 @@ package org.example.smart_delivery.repository;
 
 import org.example.smart_delivery.entity.Colis;
 import org.example.smart_delivery.entity.enums.ColisStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ColisRepository extends JpaRepository<Colis, String> {
-    List<Colis> findColisByStatut(ColisStatus statut);
+    Page<Colis> findColisByStatut(ColisStatus statut , Pageable pageable);
 }
