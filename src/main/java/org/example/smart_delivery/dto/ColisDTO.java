96eaa450d53bbccb352b87d84841ec65ca6aa696
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-08 15:04:30 +0100

    feat: update ColisDTO to enhance description length and add nullable fields for livreurId and destinataireId

--- src/main/java/org/example/smart_delivery/dto/ColisDTO.java
+++ src/main/java/org/example/smart_delivery/dto/ColisDTO.java
@@ -8,30 +8,32 @@ import lombok.Getter;
 import lombok.Setter;
 import org.example.smart_delivery.entity.enums.ColisStatus;
 import org.example.smart_delivery.entity.enums.Priority;
+import org.springframework.lang.Nullable;
 
 @Getter
 @Setter
 public class ColisDTO {
     private String id;
 
-    @Size(max = 50)
+    @Size(max = 255)
     private String description;
 
     @DecimalMin(value = "0.0", inclusive = true)
     private BigDecimal poids;
 
-    @NotNull
     private ColisStatus statut;
 
     @NotNull
     private Priority priorite;
 
     @Size(max = 50)
+    @Nullable
     private String livreurId;
 
     @Size(max = 50)
     private String clientExpediteurId;
 
     @Size(max = 50)
+    @Nullable
     private String destinataireId;
 }
