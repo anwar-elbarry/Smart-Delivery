095866e9e29abd6fb7d04042e5f6a0ca1d520ab5
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-09 17:04:04 +0100

    refactor: update ZoneService and ZoneServiceImpl to use ZoneRespDTO for response consistency

--- src/main/java/org/example/smart_delivery/service/zone/ZoneService.java
+++ src/main/java/org/example/smart_delivery/service/zone/ZoneService.java
@@ -1,12 +1,14 @@
 package org.example.smart_delivery.service.zone;
 
 import org.example.smart_delivery.dto.request.ZoneDTO;
+import org.example.smart_delivery.dto.response.ZoneRespDTO;
+
 import java.util.List;
 
 public interface ZoneService {
-    ZoneDTO create(ZoneDTO dto);
-    ZoneDTO update(String id, ZoneDTO dto);
-    ZoneDTO getById(String id);
-    List<ZoneDTO> getAll();
+    ZoneRespDTO create(ZoneDTO dto);
+    ZoneRespDTO update(String id, ZoneDTO dto);
+    ZoneRespDTO getById(String id);
+    List<ZoneRespDTO> getAll();
     void delete(String id);
 }
--- src/main/java/org/example/smart_delivery/service/zone/ZoneServiceImpl.java
+++ src/main/java/org/example/smart_delivery/service/zone/ZoneServiceImpl.java
@@ -2,8 +2,10 @@ package org.example.smart_delivery.service.zone;
 
 import lombok.RequiredArgsConstructor;
 import org.example.smart_delivery.dto.request.ZoneDTO;
+import org.example.smart_delivery.dto.response.ZoneRespDTO;
 import org.example.smart_delivery.entity.Zone;
 import org.example.smart_delivery.mapper.request.ZoneMapper;
+import org.example.smart_delivery.mapper.response.ZoneRespMapper;
 import org.example.smart_delivery.repository.ZoneRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,36 +18,37 @@ public class ZoneServiceImpl implements ZoneService {
 
     private final ZoneRepository zoneRepository;
     private final ZoneMapper zoneMapper;
+    private final ZoneRespMapper zoneRespMapper;
     @Override
     @Transactional
-    public ZoneDTO create(ZoneDTO dto) {
+    public ZoneRespDTO create(ZoneDTO dto) {
         Zone entity = zoneMapper.toEntity(dto);
         entity.setId(null);
         Zone saved = zoneRepository.save(entity);
-        return zoneMapper.toDto(saved);
+        return zoneRespMapper.toRespDto(saved);
     }
 
     @Override
     @Transactional
-    public ZoneDTO update(String id, ZoneDTO dto) {
+    public ZoneRespDTO update(String id, ZoneDTO dto) {
         Zone entity = zoneRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Zone not found: " + id));
         zoneMapper.updateEntityFromDto(dto,entity);
         Zone saved = zoneRepository.save(entity);
-        return zoneMapper.toDto(saved);
+        return zoneRespMapper.toRespDto(saved);
     }
 
     @Override
     @Transactional(readOnly = true)
-    public ZoneDTO getById(String id) {
+    public ZoneRespDTO getById(String id) {
         Zone zone = zoneRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Zone not found: " + id));
-        return  zoneMapper.toDto(zone);
+        return  zoneRespMapper.toRespDto(zone);
     }
 
     @Override
     @Transactional(readOnly = true)
-    public List<ZoneDTO> getAll() {
+    public List<ZoneRespDTO> getAll() {
         List<Zone> zoneList = zoneRepository.findAll();
-        return zoneList.stream().map(zoneMapper::toDto).toList();
+        return zoneList.stream().map(zoneRespMapper::toRespDto).toList();
     }
 
     @Override
