c4382bad68fd28fdd576aeca6d9b2589a0550231
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-09 18:39:04 +0100

    feat: add methods to filter colis by priority and zone in ColisRepository

--- src/main/java/org/example/smart_delivery/repository/ColisRepository.java
+++ src/main/java/org/example/smart_delivery/repository/ColisRepository.java
@@ -1,7 +1,9 @@
 package org.example.smart_delivery.repository;
 
 import org.example.smart_delivery.entity.Colis;
+import org.example.smart_delivery.entity.Zone;
 import org.example.smart_delivery.entity.enums.ColisStatus;
+import org.example.smart_delivery.entity.enums.Priority;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,8 @@ import java.util.List;
 
 public interface ColisRepository extends JpaRepository<Colis, String> {
     Page<Colis> findColisByStatut(ColisStatus statut , Pageable pageable);
+
+    Page<Colis> findColisByPriorite(Priority priorite,Pageable pageable);
+
+    Page<Colis> findColisByZone(Zone zone, Pageable pageable);
 }
