579f2910c30fe432bbf6d8a7283dca3d76ba0f10
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-09 19:13:00 +0100

    feat: add search method in ColisRepository for enhanced colis retrieval by various fields

--- src/main/java/org/example/smart_delivery/repository/ColisRepository.java
+++ src/main/java/org/example/smart_delivery/repository/ColisRepository.java
@@ -7,6 +7,8 @@ import org.example.smart_delivery.entity.enums.Priority;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,4 +18,23 @@ public interface ColisRepository extends JpaRepository<Colis, String> {
     Page<Colis> findColisByPriorite(Priority priorite,Pageable pageable);
 
     Page<Colis> findColisByZone(Zone zone, Pageable pageable);
+
+    @Query("""
+  select c from Colis c
+  left join c.clientExpediteur ce
+  left join c.destinataire de
+  left join c.livreur l
+  left join l.user lu
+  where
+    lower(c.description) like lower(concat('%', :q, '%'))
+    or lower(c.villeDestination) like lower(concat('%', :q, '%'))
+    or lower(ce.nom) like lower(concat('%', :q, '%'))
+    or lower(ce.prenom) like lower(concat('%', :q, '%'))
+    or lower(de.nom) like lower(concat('%', :q, '%'))
+    or lower(de.prenom) like lower(concat('%', :q, '%'))
+    or lower(lu.nom) like lower(concat('%', :q, '%'))
+    or lower(lu.prenom) like lower(concat('%', :q, '%'))
+    or c.id like concat('%', :q, '%')
+""")
+    Page<Colis> search(@Param("q") String q, Pageable pageable);
 }
