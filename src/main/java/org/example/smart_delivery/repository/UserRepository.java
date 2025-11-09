39a324c41ab9321ac86db1b1acc9d0bdc765dd5a
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-09 19:13:10 +0100

    feat: add search method in UserRepository for enhanced user retrieval by multiple fields

--- src/main/java/org/example/smart_delivery/repository/UserRepository.java
+++ src/main/java/org/example/smart_delivery/repository/UserRepository.java
@@ -1,7 +1,24 @@
 package org.example.smart_delivery.repository;
 
 import org.example.smart_delivery.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Page<User> findByNomContainingIgnoreCaseOrPrenomContainingIgnoreCaseOrTelephoneContainingOrEmailContainingIgnoreCaseOrAdressContainingIgnoreCase(
+            String nom, String prenom, String telephone, String email, String adress, Pageable pageable);
+    
+    @Query("""
+      select u from User u
+      where
+        lower(u.nom) like lower(concat('%', :q, '%'))
+        or lower(u.prenom) like lower(concat('%', :q, '%'))
+        or u.telephone like concat('%', :q, '%')
+        or lower(u.email) like lower(concat('%', :q, '%'))
+        or lower(u.adress) like lower(concat('%', :q, '%'))
+    """)
+    Page<User> search(@Param("q") String q, Pageable pageable);
 }
