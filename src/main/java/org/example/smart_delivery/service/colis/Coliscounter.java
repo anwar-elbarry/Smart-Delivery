9b36a5a670fe1c7c8fc6a71ec6f3c0f71f7141ba
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-09 20:14:45 +0100

    feat: add Coliscounter class for tracking colis count and weight

---
+++ src/main/java/org/example/smart_delivery/service/colis/Coliscounter.java
@@ -0,0 +1,19 @@
+package org.example.smart_delivery.service.colis;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+public class Coliscounter {
+    private Long count;
+    private BigDecimal poids;
+
+    public Coliscounter(Long count, BigDecimal poids) {
+        this.count = count == null ? 0L : count;
+        this.poids = poids == null ? BigDecimal.ZERO : poids;
+    }
+
+
+}
