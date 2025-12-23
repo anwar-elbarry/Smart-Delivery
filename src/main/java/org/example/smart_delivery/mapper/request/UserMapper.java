57641c25dd475a36ec505a1b6f4f9f1a96672069
anouar eil barry <elbarryanwar37@gmail.com>
2025-12-23 15:53:21 +0100

    feat: update UserMapper and UserRespMapper to include role and verification code mappings

--- src/main/java/org/example/smart_delivery/mapper/request/UserMapper.java
+++ src/main/java/org/example/smart_delivery/mapper/request/UserMapper.java
@@ -3,16 +3,20 @@ package org.example.smart_delivery.mapper.request;
 import org.example.smart_delivery.dto.request.UserDTO;
 import org.example.smart_delivery.entity.User;
 import org.example.smart_delivery.mapper.RefMapper;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses = RefMapper.class)
 public interface UserMapper {
+    @Mapping(target = "roleId", source = "role.id")
     UserDTO toDto(User entity);
+    @Mapping(target = "verificationCodeExpired", ignore = true)
+    @Mapping(target = "verificationCode", ignore = true)
+    @Mapping(target = "role.id", source = "roleId")
     User toEntity(UserDTO dto);
 
+    @Mapping(target = "role.id", source = "roleId")
+    @Mapping(target = "verificationCodeExpired", ignore = true)
+    @Mapping(target = "verificationCode", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDto(UserDTO dto, @MappingTarget User entity);
 }
--- src/main/java/org/example/smart_delivery/mapper/response/UserRespMapper.java
+++ src/main/java/org/example/smart_delivery/mapper/response/UserRespMapper.java
@@ -2,17 +2,15 @@ package org.example.smart_delivery.mapper.response;
 
 import org.example.smart_delivery.dto.response.UserRespDTO;
 import org.example.smart_delivery.entity.User;
-import org.example.smart_delivery.mapper.RefMapper;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", uses = RefMapper.class)
+@Mapper(componentModel = "spring")
 public interface UserRespMapper {
+    @Mapping(target = "roleName", source = "role.name")
     UserRespDTO toRespDto(User entity);
+    @Mapping(target = "verificationCodeExpired", ignore = true)
+    @Mapping(target = "verificationCode", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "role.name", source = "roleName")
     User toRespEntity(UserRespDTO dto);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void updateRespEntityFromDto(UserRespDTO dto, @MappingTarget User entity);
 }
