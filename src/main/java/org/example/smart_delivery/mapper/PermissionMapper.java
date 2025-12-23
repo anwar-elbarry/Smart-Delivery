cca118a2029d3c82b5d79856d49c5a1022c1a43d
anouar eil barry <elbarryanwar37@gmail.com>
2025-12-23 15:53:01 +0100

    feat: update PermissionMapper to ignore id field during entity mapping

--- src/main/java/org/example/smart_delivery/mapper/PermissionMapper.java
+++ src/main/java/org/example/smart_delivery/mapper/PermissionMapper.java
@@ -4,9 +4,11 @@ import org.example.smart_delivery.dto.request.PermissionReqDTO;
 import org.example.smart_delivery.dto.response.PermissionResDTO;
 import org.example.smart_delivery.entity.Permission;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PermissionMapper {
+    @Mapping(target = "id", ignore = true)
     Permission toEntity(PermissionReqDTO reqDTO);
     PermissionResDTO toResp(Permission entity);
 }
